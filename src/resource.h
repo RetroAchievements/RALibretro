a1d2db90e424218b6befb1cf2d93838caf244a82
leiradel <andre@leiradella.com>
2018-02-18 15:35:32 +0000

    Forgot those...

--- src/resource.h
+++ src/resource.h
@@ -2,45 +2,51 @@
 #define IDC_STATIC (-1)
 #endif
 
-#define IDM_SYSTEM_STELLA                       40000
+// Keep those in increasing order
+#define IDM_SYSTEM_STELLA                       50000
+#define IDM_SYSTEM_SNES9X                       50001
+#define IDM_SYSTEM_PICODRIVE                    50002
+#define IDM_SYSTEM_GENESISPLUSGX                50003
+#define IDM_SYSTEM_FCEUMM                       50004
+#define IDM_SYSTEM_HANDY                        50005
+#define IDM_SYSTEM_BEETLESGX                    50006
+#define IDM_SYSTEM_GAMBATTE                     50007
+#define IDM_SYSTEM_MGBA                         50008
+#define IDM_SYSTEM_MEDNAFENPSX                  50009
+#define IDM_SYSTEM_MEDNAFENNGP                  50010
+
+// Keep those in increasing order
+#define IDM_SAVE_STATE_0                        51000
+#define IDM_SAVE_STATE_1                        51001
+#define IDM_SAVE_STATE_2                        51002
+#define IDM_SAVE_STATE_3                        51003
+#define IDM_SAVE_STATE_4                        51004
+#define IDM_SAVE_STATE_5                        51005
+#define IDM_SAVE_STATE_6                        51006
+#define IDM_SAVE_STATE_7                        51007
+#define IDM_SAVE_STATE_8                        51008
+#define IDM_SAVE_STATE_9                        51009
+
+// Keep those in increasing order
+#define IDM_LOAD_STATE_0                        52000
+#define IDM_LOAD_STATE_1                        52001
+#define IDM_LOAD_STATE_2                        52002
+#define IDM_LOAD_STATE_3                        52003
+#define IDM_LOAD_STATE_4                        52004
+#define IDM_LOAD_STATE_5                        52005
+#define IDM_LOAD_STATE_6                        52006
+#define IDM_LOAD_STATE_7                        52007
+#define IDM_LOAD_STATE_8                        52008
+#define IDM_LOAD_STATE_9                        52009
+
 #define IDM_LOAD_GAME                           40001
-#define IDM_SAVE_STATE_0                        40002
-#define IDM_SAVE_STATE_1                        40003
-#define IDM_SAVE_STATE_2                        40004
-#define IDM_SAVE_STATE_3                        40005
-#define IDM_SAVE_STATE_4                        40006
-#define IDM_SAVE_STATE_5                        40007
-#define IDM_SAVE_STATE_6                        40008
-#define IDM_SAVE_STATE_7                        40009
-#define IDM_SAVE_STATE_8                        40010
-#define IDM_SAVE_STATE_9                        40011
-#define IDM_LOAD_STATE_0                        40012
-#define IDM_LOAD_STATE_1                        40013
-#define IDM_LOAD_STATE_2                        40014
-#define IDM_LOAD_STATE_3                        40015
-#define IDM_LOAD_STATE_4                        40016
-#define IDM_LOAD_STATE_5                        40017
-#define IDM_LOAD_STATE_6                        40018
-#define IDM_LOAD_STATE_7                        40019
-#define IDM_LOAD_STATE_8                        40020
-#define IDM_LOAD_STATE_9                        40021
-#define IDM_RESET_GAME                          40022
-#define IDM_EXIT                                40023
-#define IDM_CORE                                40024
-#define IDM_INPUT                               40025
-#define IDM_ABOUT                               40026
-#define IDM_CLOSE_GAME                          40027
-#define IDM_PAUSE_GAME                          40028
-#define IDM_RESUME_GAME                         40029
-#define IDM_SYSTEM_SNES9X                       40030
-#define IDM_SYSTEM_PICODRIVE                    40031
-#define IDM_TURBO                               40032
-#define IDM_SYSTEM_GENESISPLUSGX                40033
-#define IDM_SYSTEM_FCEUMM                       40034
-#define IDM_SYSTEM_HANDY                        40035
-#define IDM_SYSTEM_BEETLESGX                    40036
-#define IDM_SYSTEM_GAMBATTE                     40037
-#define IDM_SYSTEM_MGBA                         40038
-#define IDM_SYSTEM_MEDNAFENPSX                  40039
-#define IDM_SYSTEM_MEDNAFENNGP                  40040
-#define IDM_CLOSE_CORE                          40041
+#define IDM_RESET_GAME                          40002
+#define IDM_EXIT                                40003
+#define IDM_CORE                                40004
+#define IDM_INPUT                               40005
+#define IDM_ABOUT                               40006
+#define IDM_CLOSE_GAME                          40007
+#define IDM_PAUSE_GAME                          40008
+#define IDM_RESUME_GAME                         40009
+#define IDM_TURBO_GAME                          40010
+#define IDM_CLOSE_CORE                          40011
