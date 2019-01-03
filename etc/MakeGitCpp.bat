95bc6f380db843aae0afa2ee003abc6032a934aa
Jamiras <jamirasmt@gmail.com>
2019-01-02 18:25:59 -0700

    also generate gitVersion when generating Git.cpp in MSVC build

--- etc/MakeGitCpp.bat
+++ etc/MakeGitCpp.bat
@@ -7,13 +7,28 @@ git rev-parse HEAD > Temp.txt
 set /p FULLHASH=<Temp.txt
 set MINIHASH=%FULLHASH:~0,7%
 
+git describe --tags > Temp.txt
+set /p VERSION_TAG=<Temp.txt
+for /f "tokens=1,2 delims=-" %%a in ("%VERSION_TAG%") do set VERSION_NUM=%%a&set VERSION_REVISION=%%b
+
+if "%VERSION_REVISION%" neq "" set VERSION_NUM=%VERSION_NUM%.%VERSION_REVISION%
+
+echo Tag: %VERSION_TAG% (%VERSION_NUM%)
+
+
+
 if exist Git.cpp del Git.cpp
 setlocal enabledelayedexpansion
-for /f "tokens=* delims=" %%i in (%TEMPLATE%) do (
-    set LINE=%%i
-    set LINE=!LINE:GITMINIHASH=%MINIHASH%!
-    set LINE=!LINE:GITFULLHASH=%FULLHASH%!
-    echo !LINE! >> Git.cpp
+for /f "tokens=1* delims=]" %%a in ('type "%TEMPLATE%" ^| find /V /N ""') do (
+    set "LINE=%%b"
+    if "!LINE!"=="" (
+        echo. >> Git.cpp
+    ) else (
+        set LINE=!LINE:GITMINIHASH=%MINIHASH%!
+        set LINE=!LINE:GITFULLHASH=%FULLHASH%!
+        set LINE=!LINE:GITRELEASE=%VERSION_NUM%!
+        echo !LINE! >> Git.cpp
+    )
 )
 
 if not exist ..\src\Git.cpp goto nonexistant
