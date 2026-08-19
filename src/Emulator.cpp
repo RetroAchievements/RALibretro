c02ae45698481b80639027f11c09e2307892c5b6
televandalist <45054151+televandalist@users.noreply.github.com>
2026-08-19 06:52:56 -0500

    Update Emulator.cpp

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -380,7 +380,7 @@ const char* getSystemManufacturer(int system)
       return "Commodore";
 
     case RC_CONSOLE_SHARPX1:
-    case RC_CONSOLE_X68000:
+    case RC_CONSOLE_X68K:
       return "Sharp";
 
     case RC_CONSOLE_ZX81:
